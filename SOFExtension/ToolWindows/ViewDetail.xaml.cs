9307dab86ef04d6c8adf31c69cb2c419195e3452
Neil Earlam <neil.earlam@lawware.co.uk>
2019-12-13 09:11:25 +0000

    Tidied up some code.

--- SOFExtension/ToolWindows/ViewDetail.xaml.cs
+++ SOFExtension/ToolWindows/ViewDetail.xaml.cs
@@ -10,94 +10,95 @@ using System.Windows.Controls;
 
 namespace SOFExtension.ToolWindows
 {
-    /// <summary>
-    /// Interaction logic for ViewDetail.xaml
-    /// </summary>
-    public partial class ViewDetail : UserControl
-    {
-        private readonly long _questionId;
-        private readonly string _query;
+	/// <summary>
+	/// Interaction logic for ViewDetail.xaml
+	/// </summary>
+	public partial class ViewDetail : UserControl
+	{
+		private readonly long _questionId;
+		private readonly string _query;
 
-        private Client _client;
+		private Client _client;
 
-        public ViewDetail(long questionId, string query)
-        {
-            InitializeComponent();
+		public ViewDetail( long questionId, string query )
+		{
+			InitializeComponent();
 
-            _questionId = questionId;
-            _query = query;
+			_questionId = questionId;
+			_query = query;
 
-            OnStart();
-        }
+			OnStart();
+		}
 
-        private void OnStart()
-        {
-            if (!LoadFromCache()) {
-                LoadQuestionFromStackoverflow();
-            }
-        }
+		private void OnStart()
+		{
+			if( !LoadFromCache() ) {
+				LoadQuestionFromStackoverflow();
+			}
+		}
 
-        private bool LoadFromCache()
-        {
-            var model = NaiveCache.GetQuestionModel(_questionId);
-            if(model != null) {
-                txtTitle.Text = model.Question.Title;
-                txtBody.Text = model.Question.BodyMarkdown;
-                hlLink.NavigateUri = new Uri( model.Question.Link);
-                icAnswers.ItemsSource = model.Answers;
-                return true;
-            }
-            return false;
-        }
+		private bool LoadFromCache()
+		{
+			var model = NaiveCache.GetQuestionModel( _questionId );
+			if( model != null ) {
+				txtTitle.Text = HtmlParser( model.Question.Title );
+				txtBody.Text = HtmlParser( model.Question.BodyMarkdown );
+				btnGotToSite.Tag = model.Question.Link;
+				icAnswers.ItemsSource = model.Answers;
+				return true;
+			}
+			return false;
+		}
 
-        private async Task LoadQuestionFromStackoverflow()
-        {
-            _client = new Client();
-            var result = await _client.GetQuestionResultAsync( _questionId );
-           
-            var model = result.Items.FirstOrDefault();
-            model = HtmlParser( model );
-            txtTitle.Text = model.Title;
-            txtBody.Text = model.BodyMarkdown;
-            hlLink.NavigateUri = new Uri( model.Link );
+		private async Task LoadQuestionFromStackoverflow()
+		{
+			_client = new Client();
+			var result = await _client.GetQuestionResultAsync( _questionId );
 
-            QuestionCacheModel cacheModel = new QuestionCacheModel() {
-                QuestionId = model.QuestionId,
-                AddedOn = DateTime.Now,
-                Question = model
-            };
+			var model = result.Items.FirstOrDefault();
+			txtTitle.Text = HtmlParser( model.Title );
+			txtBody.Text = HtmlParser( model.BodyMarkdown );
+			btnGotToSite.Tag = model.Link;
 
-            LoadAnswersFromStackoverflow( cacheModel );
-        }
+			var cacheModel = new QuestionCacheModel() {
+				QuestionId = model.QuestionId,
+				AddedOn = DateTime.Now,
+				Question = model
+			};
 
-        private async Task LoadAnswersFromStackoverflow(QuestionCacheModel cacheModel)
-        {
-            _client = new Client();
-            var result = await _client.GetAnswersResultAsync( _questionId );
-            icAnswers.ItemsSource = result.Items;
-            cacheModel.Answers = result.Items;
-            NaiveCache.AddQuestionModel( cacheModel );
-        }
+			LoadAnswersFromStackoverflow( cacheModel );
+		}
 
-        private SOFQuestionModel.Item HtmlParser(SOFQuestionModel.Item model)
-        {
-            model.Title = WebUtility.HtmlDecode( model.Title );
-            model.BodyMarkdown = WebUtility.HtmlDecode( model.BodyMarkdown );
-            return model;
-        }
+		private async Task LoadAnswersFromStackoverflow( QuestionCacheModel cacheModel )
+		{
+			_client = new Client();
+			var result = await _client.GetAnswersResultAsync( _questionId );
+			foreach( var item in result.Items ) {
+				item.BodyMarkdown = HtmlParser( item.BodyMarkdown );
+			}
+			icAnswers.ItemsSource = result.Items;
+			cacheModel.Answers = result.Items;
+			NaiveCache.AddQuestionModel( cacheModel );
+		}
 
-        private void btnBack_Click(object sender, RoutedEventArgs e)
-        {
-            this.Content = new SOFMainWindowControl(_query);
-        }
+		private string HtmlParser( string data )
+		{
+			return WebUtility.HtmlDecode( data );
+		}
 
-        private void Hyperlink_RequestNavigate( object sender, System.Windows.Navigation.RequestNavigateEventArgs e )
-        {
-            var ps = new ProcessStartInfo( e.Uri.AbsoluteUri ) {
-                UseShellExecute = true,
-                Verb = "open"
-            };
-            Process.Start( ps );
-        }
-    }
+		private void btnBack_Click( object sender, RoutedEventArgs e )
+		{
+			this.Content = new SOFMainWindowControl( _query );
+		}
+
+		private void btnGotToSite_Click( object sender, RoutedEventArgs e )
+		{
+			var link = ( (Button)sender ).Tag;
+			var ps = new ProcessStartInfo( (string)link ) {
+				UseShellExecute = true,
+				Verb = "open"
+			};
+			Process.Start( ps );
+		}
+	}
 }
